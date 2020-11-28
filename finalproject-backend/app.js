742f41bf40f069689a605a13cf472558db9f6855
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-11-27 22:09:16 -0500

    added heroku to backend

--- finalproject-backend/app.js
+++ finalproject-backend/app.js
@@ -1,1 +1,30 @@
 // backend application for final
+const express = require("express"); // "npm install express" to install express
+const app = express();
+const port = process.env.PORT || 4000;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLz_TlYlo-Zs9hcVzVbFt52PJrsaNfUMw",
+  authDomain: "final-project-7bf99.firebaseapp.com",
+  databaseURL: "https://final-project-7bf99.firebaseio.com",
+  projectId: "final-project-7bf99",
+  storageBucket: "final-project-7bf99.appspot.com",
+  messagingSenderId: "658834115173",
+  appId: "1:658834115173:web:81ca21cbdebb9cfbf2948c",
+  measurementId: "G-3ZKJ2EJFMK",
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+const indexRoute = require("./routes/index.js");
+const postRoute = require("./routes/post.js");
+const createRoute = require("./routes/createBlogpost.js");
+
+app.use("/", indexRoute);
+app.use("/post", postRoute);
+app.use("/create", createRoute);
+
+app.listen(port, () =>
+  console.log(`Exercise Four is running at local host:${port}`)
+);
---
+++ finalproject-backend/routes/createPost.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+// Require Firebase
+const firebase = require("firebase");
+// Initialize Firebase Database
+const db = firebase.firestore();
+// Reference a specific collection
+const blogposts = db.collection("blogposts");
+
+const form = `
+<form action="/create/submit">
+    <input type="text" name="title" placeholder="Title of Post"/>
+    <input type="text" name="text" placeholder="Text of Post"/>
+    <input type="text" name="author" placeholder="Author"/>
+    <button type="submit">Submit Post</button>
+</form>
+`;
+
+// default route serves form
+router.get("/", (req, res) => res.send(form));
+
+// route for submitting form
+router.get("/submit", (req, res) => {
+  const queryParams = req.query;
+  // custom id for our posts
+  const idFromTitle = queryParams.title.replace(/\s+/g, "-").toLowerCase();
+  // cool post = cool-post
+  blogposts
+    .doc(idFromTitle) // allows you to create new posts or update them
+    .set(queryParams)
+    .then(function (doc) {
+      res.send("Successful Submission");
+    })
+    .catch(function (error) {
+      console.log("error", error);
+      res.send("Failed Submission");
+    });
+});
+
+module.exports = router;
